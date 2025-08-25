c8ccc226c6053cabed83e3154dc247f0cdf114cc
Brain API Developer <developer@brain-api.local>
2025-08-25 13:39:44 +0900

    fix: Update default port from 8000 to 8080 in main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(); // CORS 활성화
 
-  const port = process.env.PORT || 8000;
+  const port = process.env.PORT || 8080;
   await app.listen(port);
 }
 bootstrap();
